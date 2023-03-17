0fbe31567618b9cbbc1db725e6f59307be7ad3d6
iam_petch <ratchanon788@gmail.com>
2023-03-17 23:31:33 +0700

    AAA

--- src/User.js
+++ src/User.js
@@ -141,7 +141,7 @@ export default function User() {
         >
           
           <Typography  variant="h3">
-            Click next Project
+            ClickNext Project
           </Typography>
 
           </Box>
